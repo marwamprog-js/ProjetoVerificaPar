6b45beb0febca74261f8a4cc42c337451195422a
Marwam <>
2017-04-25 21:38:18 -0300

    Classe que verifica se é par ou impar

---
+++ ProjectParImpar/src/projectparimpar/ProjectParImpar.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectparimpar;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author user_mw
+ */
+public class ProjectParImpar {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int num1 = 0, num2 = 0;
+        
+        num1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro numero.: "));
+        num2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo numero.: "));
+        
+        
+        
+        
+    }
+    
+}
